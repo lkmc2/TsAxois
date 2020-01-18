47492c5bac639872ebaee859c9b2c4ce4630e195
lkmc2 <lkmc2@163.com>
2020-01-18 16:57:56 +0800

    设置服务器路由返回的 json 数据

--- examples/server.js
+++ examples/server.js
@@ -23,6 +23,17 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const router = express.Router()
+
+// 设置路由返回的 json 数据
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: 'hello world'
+  })
+})
+
+app.use(router)
+
 const port = process.env.PORT || 8099
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
